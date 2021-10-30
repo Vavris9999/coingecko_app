b6ee045cd83c6d66652d6bf8096ff7072df240fd
Vavris <mikimouse2356@gmail.com>
2021-10-30 11:59:57 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'semestralni_projekt',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
